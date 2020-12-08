4084ec9dab09afe6259da8571f0030990eb18935
WKFilipeDavi <filipedavigarcia@hotmail.com>
2020-12-08 14:29:15 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Portfólio com links para outros sites
+
+Como usar:
+
+Baixe os arquivos, abra a página "index.html" e navegue.
+
+
+#Portfolio with links to other sites
+
+How to use:
+
+Download the files, open the "index.html" page and browse.
---
+++ script.js
@@ -0,0 +1,15 @@
+//console.log(cards.length);
+
+//cards[1].style.height = "100px";
+
+/*(function(){
+
+    var cards = document.getElementsByClassName("blueborder");
+    let i;
+    for(i = 0; i < cards.length; i++)
+    {
+        cards[i].style.height = cards[1].style.height;
+        alert(cards[i].style.height);
+    }
+
+})();*/
